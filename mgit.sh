1d371e4748719db2461a7c532b9fdc57b49995c8
bidaya0 <xianzelin984@outlook.com>
2021-08-05 15:52:54 +0800

    init bash to warp git

---
+++ mgit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : bidaya0 (bidaya0@$HOSTNAME)
+# @file        : mgit
+# @created     : Thursday Aug 05, 2021 15:26:51 CST
+#
+# @description : 
+######################################################################
+
+merge-sub-gits local
+merge-sub-gits origin -l
