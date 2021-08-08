7691810eda9051ae68dee13327557dda96288479
bidaya0 <xianzelin984@outlook.com>
2021-08-08 23:14:30 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : bidaya0 (bidaya0@$HOSTNAME)
+# @file        : init
+# @created     : Sunday Aug 08, 2021 23:11:59 CST
+#
+# @description : 
+######################################################################
+
+
+ln ./.vimrc ~/.vimrc
+ln -s ./vim ~/.vim
