a784f984e70564d0b7cc2ce30ae1479ee97d75b4
bidaya0 <xianzelin984@outlook.com>
2022-02-09 00:33:58 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -12,9 +12,7 @@
 ln ./.vimrc ~/.vimrc
 ln -s `realpath ./.vim-templates` `realpath ~/.vim-templates`
 if [ "$VIM_USE_PROXY" = "TRUE" ]; then
-		echo "get it"
 		proxychains4 -q vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 else
-		echo "hello"
 		vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 fi
